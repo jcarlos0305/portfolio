ef66c75483803c6d51e6d307d733a2e82d4f8a3f
Juan Carlos Sanchez <jsanchez@konradgroup.com>
2019-03-16 02:08:47 -0600

    Refactor

--- src/app/utils/credentials.ts
+++
@@ -1,5 +0,0 @@
-export const Credentials = {
-    API_KEY : 'AIzaSyA0G2Pcd9HLBYTVfKOn4dfZkJy1MK2HSlo',
-    AUTH_KEY : '775808774552-btkculje7au7sr9b73n2t3kli4t1717t.apps.googleusercontent.com',
-    AUTH_SECRET : '_MYmG1SP09cUcTbDaBfH93iA'
-}
